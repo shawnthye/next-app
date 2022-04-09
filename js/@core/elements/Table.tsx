dfd99de69fb850b4ccbd4b51706b0a2aeae41e5a
shawnthye <shawn.thye@gmail.com>
2022-04-10 00:16:47 +0800

    - cleanup

--- js/@core/elements/Table.tsx
+++ js/@core/elements/Table.tsx
@@ -103,7 +103,7 @@ const headCells: readonly HeadCell[] = [
   {
     id: 'name',
     numeric: false,
-    label: 'Dessert (100g serving)',
+    label: 'Dessert (100g serving)',
   },
   {
     id: 'calories',
@@ -113,27 +113,25 @@ const headCells: readonly HeadCell[] = [
   {
     id: 'fat',
     numeric: true,
-    label: 'Fat (g)',
+    label: 'Fat (g)',
   },
   {
     id: 'carbs',
     numeric: true,
-    label: 'Carbs (g)',
+    label: 'Carbs (g)',
   },
   {
     id: 'protein',
     numeric: true,
-    label: 'Protein (g)',
+    label: 'Protein (g)',
   },
 ];
 
 interface EnhancedTableProps {
-  numSelected: number;
   onRequestSort: (
     event: React.MouseEvent<unknown>,
     property: keyof Data,
   ) => void;
-  onSelectAllClick: (event: React.ChangeEvent<HTMLInputElement>) => void;
   order: Order;
   orderBy: string;
   rowCount: number;
@@ -172,17 +170,11 @@ function EnhancedTableHead(props: EnhancedTableProps) {
   );
 }
 
-interface EnhancedTableToolbarProps {
-  numSelected: number;
-}
-
 const Table: NextPage = () => {
   const [order, setOrder] = React.useState<Order>('asc');
   const [orderBy, setOrderBy] = React.useState<keyof Data>('calories');
-  const [selected, setSelected] = React.useState<readonly string[]>([]);
   const [page, setPage] = React.useState(0);
-  const [dense, setDense] = React.useState(false);
-  const [rowsPerPage, setRowsPerPage] = React.useState(5);
+  const [rowsPerPage, setRowsPerPage] = React.useState(10);
 
   const handleRequestSort = (
     _: React.MouseEvent<unknown>,
@@ -191,35 +183,6 @@ const Table: NextPage = () => {
     const isAsc = orderBy === property && order === 'asc';
     setOrder(isAsc ? 'desc' : 'asc');
     setOrderBy(property);
-  };
-
-  const handleSelectAllClick = (event: React.ChangeEvent<HTMLInputElement>) => {
-    if (event.target.checked) {
-      const newSelecteds = rows.map(n => n.name);
-      setSelected(newSelecteds);
-      return;
-    }
-    setSelected([]);
-  };
-
-  const handleClick = (event: React.MouseEvent<unknown>, name: string) => {
-    const selectedIndex = selected.indexOf(name);
-    let newSelected: readonly string[] = [];
-
-    if (selectedIndex === -1) {
-      newSelected = newSelected.concat(selected, name);
-    } else if (selectedIndex === 0) {
-      newSelected = newSelected.concat(selected.slice(1));
-    } else if (selectedIndex === selected.length - 1) {
-      newSelected = newSelected.concat(selected.slice(0, -1));
-    } else if (selectedIndex > 0) {
-      newSelected = newSelected.concat(
-        selected.slice(0, selectedIndex),
-        selected.slice(selectedIndex + 1),
-      );
-    }
-
-    setSelected(newSelected);
   };
 
   const handleChangePage = (event: unknown, newPage: number) => {
@@ -233,12 +196,6 @@ const Table: NextPage = () => {
     setPage(0);
   };
 
-  const handleChangeDense = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setDense(event.target.checked);
-  };
-
-  const isSelected = (name: string) => selected.indexOf(name) !== -1;
-
   // Avoid a layout jump when reaching the last page with empty rows.
   const emptyRows =
     page > 0 ? Math.max(0, (1 + page) * rowsPerPage - rows.length) : 0;
@@ -250,12 +207,10 @@ const Table: NextPage = () => {
           <MuiTable
             sx={{ minWidth: 750 }}
             aria-labelledby="tableTitle"
-            size={dense ? 'small' : 'medium'}>
+            size={'medium'}>
             <EnhancedTableHead
-              numSelected={selected.length}
               order={order}
               orderBy={orderBy}
-              onSelectAllClick={handleSelectAllClick}
               onRequestSort={handleRequestSort}
               rowCount={rows.length}
             />
@@ -265,7 +220,6 @@ const Table: NextPage = () => {
               {stableSort(rows, getComparator(order, orderBy))
                 .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
                 .map((row, index) => {
-                  const isItemSelected = isSelected(row.name);
                   const labelId = `enhanced-table-checkbox-${index}`;
 
                   return (
@@ -273,7 +227,7 @@ const Table: NextPage = () => {
                       hover
                       tabIndex={-1}
                       key={row.name}
-                      selected={isItemSelected}>
+                      selected={false}>
                       <TableCell component="th" id={labelId} scope="row">
                         {row.name}
                       </TableCell>
@@ -287,7 +241,7 @@ const Table: NextPage = () => {
               {emptyRows > 0 && (
                 <TableRow
                   style={{
-                    height: (dense ? 33 : 53) * emptyRows,
+                    height: 53 * emptyRows,
                   }}>
                   <TableCell colSpan={6} />
                 </TableRow>
@@ -296,7 +250,7 @@ const Table: NextPage = () => {
           </MuiTable>
         </TableContainer>
         <TablePagination
-          rowsPerPageOptions={[5, 10, 25]}
+          rowsPerPageOptions={[10, 25, 50]}
           component="div"
           count={rows.length}
           rowsPerPage={rowsPerPage}
